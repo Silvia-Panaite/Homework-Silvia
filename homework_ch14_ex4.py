439ea8554c6c3f1b0809b1fc31bdcd13c3aa0446
Silvia <silvia.panaite2014@yahoo.ro>
2023-02-13 22:00:29 +0200

    'add'

--- homework_ch14_ex4.py
+++ homework_ch14_ex4.py
@@ -1,19 +1,18 @@
+n = int(input('Please type in an integer number: '))
+
 def proper_divisor(n: int) -> list:
     list_ = []
     for i in range(1, (n//2) + 1):
         if n % i == 0:
-            list.append(i)
+            list_.append(i)
     return list_
 
+print(proper_divisor(n))
+
 def main():
-    n = int(input('Please enter a positiver integer: '))
+    n = int(input('Please enter a positive integer: '))
     print('The list of proper divisors of', n, 'is:')
     print(proper_divisor(n))
-
-if __name__ == '_main_':
-    main()
     
-
-
 
 
