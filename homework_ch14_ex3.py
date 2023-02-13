0e74b4eb83a625cb43dc656cf3038949001d8ba0
Silvia <silvia.panaite2014@yahoo.ro>
2023-02-13 17:56:50 +0200

    'add'

---
+++ homework_ch14_ex3.py
@@ -0,0 +1,25 @@
+list1 = []
+
+while True:
+    number = int(input('Please type in a number: '))
+    if number < 0:
+        list = list + [number]
+        list.sort()
+        print('The list with negatives is: ')
+        for item in list:
+            print(' - ' + str(item))
+    if number == 0:
+        list = list + [number]
+        list.sort()
+        print('The list with 0 is: ')
+        for item in list:
+            print(' - ' + str(item))
+    if number > 0:
+        list = list + [number]
+        list.sort()
+        print('The list with positives is: ')
+        for item in list:
+            print(' - ' + str(item))
+
+
+
